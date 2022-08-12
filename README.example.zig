bde0d2deff6d54b4000e241056912f34a68fe43a
shunkeen <shunki.nakamura.1990@gmail.com>
2022-08-12 14:36:09 +0900

    update README example code

--- README.example.zig
+++ README.example.zig
@@ -6,15 +6,15 @@ fn even(x: usize) bool {
 }
 
 const RepeatTake = zignite.Repeat(usize).Take();
-fn repeat_take(x: usize) RepeatTake {
+fn repeatTake(x: usize) RepeatTake {
     return zignite.repeat(usize, x).take(x);
 }
 
 test "Example Code" {
     const x = zignite
-        .range(usize, 0, 100) //              { 0, 1, ..., 99 }
-        .filter(even) //                      { 0, 2, ..., 98 }
-        .flatMap(RepeatTake, repeat_take) // { 2, 2, 4, 4, 4, 4, ..., 98 }
+        .range(usize, 0, 100) //            { 0, 1, ..., 99 }
+        .filter(even) //                    { 0, 2, ..., 98 }
+        .flatMap(RepeatTake, repeatTake) // { 2, 2, 4, 4, 4, 4, ..., 98 }
         .sum();
 
     try std.testing.expect(x == 161700);
