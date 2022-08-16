0040725ae8cd5531279d8698e423ab97795c8862
shunkeen <90960837+shunkeen@users.noreply.github.com>
2022-08-16 17:01:54 +0900

    update ObverseIndex to use inline function

--- src/producer/obverse_index.zig
+++ src/producer/obverse_index.zig
@@ -16,9 +16,8 @@ pub fn ObverseIndex(comptime S: type, comptime T: type, comptime len: fn (state:
         pub fn next(event: Type.Event) Type.Action {
             const s = event.state;
             const i = event.index;
-            const a_i = .{ .modifier = .always_inline };
-            if (0 <= i and i < @call(a_i, len, .{s})) {
-                return Type.Action._yield(_init(s, i + 1), @call(a_i, get, .{ s, i }));
+            if (0 <= i and i < _len(s)) {
+                return Type.Action._yield(_init(s, i + 1), _get(s, i));
             } else {
                 return Type.Action._break(_init(s, i));
             }
@@ -28,6 +27,14 @@ pub fn ObverseIndex(comptime S: type, comptime T: type, comptime len: fn (state:
 
         inline fn _init(state: S, index: usize) Type.State {
             return .{ .state = state, .index = index };
+        }
+
+        inline fn _len(state: S) usize {
+            return @call(.{ .modifier = .always_inline }, len, .{state});
+        }
+
+        inline fn _get(state: S, index: usize) T {
+            return @call(.{ .modifier = .always_inline }, get, .{ state, index });
         }
     };
 }
